b6a1cd3b59b2446b12f7021436d7d16beff4f503
Shurc1val <hughpm16@gmail.com>
2023-09-30 15:53:38 +0100

    Console version of game works

---
+++ TRS_Graphics.py
@@ -0,0 +1,128 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter.font import Font
+
+CANVAS = {'height': 800, 'width': 650}
+BOARD_SQUARE = {'width': 70, 'spacer': 5}
+NUM_SQUARES = 8
+GAP_FROM_EDGE = (CANVAS['width'] - NUM_SQUARES *
+                 (BOARD_SQUARE['width']+BOARD_SQUARE['spacer']) + BOARD_SQUARE['spacer'])/2
+
+GAP_FROM_TOP = GAP_FROM_EDGE
+
+CURRENT_SQUARE = -1
+
+
+def piece_selected(*args):
+    global CURRENT_SQUARE
+    CURRENT_SQUARE += 1
+    x, y = board_number_to_position(CURRENT_SQUARE)
+    canvas.moveto(canvas.find_withtag("current"), x, y)
+
+
+def board_number_to_position(place_index: int) -> int:
+    if place_index < 7:
+        x_coord = GAP_FROM_EDGE + 7 * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+        y_coord = GAP_FROM_TOP + (7 - place_index) * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+
+    elif place_index < 15:
+        place_index -= 7
+        x_coord = GAP_FROM_EDGE + (7 - place_index) * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+        y_coord = GAP_FROM_TOP
+
+    elif place_index < 22:
+        place_index -= 14
+        x_coord = GAP_FROM_EDGE
+        y_coord = GAP_FROM_TOP + place_index * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+
+    else:
+        place_index -= 21
+        x_coord = GAP_FROM_EDGE + place_index * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+        y_coord = GAP_FROM_TOP + 7 * \
+            (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'])
+
+    return x_coord, y_coord
+
+
+def draw_board(canvas: Canvas, board: list[list[str]]):
+
+
+
+
+players = ["Red"]
+current_player = "Red"
+
+counters = {}
+for colour in players:
+    counters[colour] = []
+num_counters = 1
+
+window = Tk()
+
+window.title('The Roman Stones')
+window.geometry("720x800+10+20")
+window.configure(bg="black")
+
+canvas = Canvas(window, width=CANVAS['width'],
+                height=CANVAS['height'], background="black")
+
+count = 0
+
+place_numbering = ["S", "XII", "XI", "X", "IX", "IIX", "VII", "S", "S", "XIX", "XX", "XXI", "XXII", "XXIII",
+                   "XXIV", "S", "S", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "S", "S", "VI", "V", "IV", "III", "II", "I", "S"]
+for i in range(2):
+    for j in range(2):
+        for k in range(8):
+            r = 5
+            x_1 = (1-i)*k*(BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + i*j * \
+                7*(BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + GAP_FROM_EDGE
+            y_1 = i*k*(BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + (1-i)*j*7 * \
+                (BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + GAP_FROM_TOP
+            x_2 = (1-i)*(k+1)*BOARD_SQUARE['width'] + (1-i)*k*BOARD_SQUARE['spacer'] + i * \
+                BOARD_SQUARE['width'] + i*j*7*(BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + GAP_FROM_EDGE
+            y_2 = GAP_FROM_TOP + (1-i)*BOARD_SQUARE['width'] + (1-i)*j*7*(
+                BOARD_SQUARE['width'] + BOARD_SQUARE['spacer']) + i*(k+1)*BOARD_SQUARE['width'] + i*k*BOARD_SQUARE['spacer']
+            points = (x_1, y_1, x_2, y_2)
+            canvas.create_rectangle(points, fill="white")
+            canvas.create_text(
+                (x_1+x_2)/2, (y_1+y_2)/2, text=place_numbering[count], font=Font(family="Times New Roman", size=20))
+            count += 1
+            
+
+for colour in players:
+    for i in range(1, num_counters+1):
+        counters[colour].append(canvas.create_oval(
+            20, 20, 40, 40, fill="red", tags=colour + str(i)))
+        canvas.tag_bind(colour + str(i), "<Button-1>", piece_selected)
+
+
+board_centre = Image.open(
+    "/Users/hughmorris/Documents/Prework/capstone-project/images/bc.png")
+
+resized_image = board_centre.resize(
+    (6*(BOARD_SQUARE['width']+BOARD_SQUARE['spacer']) - 2*BOARD_SQUARE['spacer'], 6*(BOARD_SQUARE['width']+BOARD_SQUARE['spacer']) - 2*BOARD_SQUARE['spacer']))
+new_image = ImageTk.PhotoImage(resized_image)
+canvas.create_image(canvas.winfo_reqwidth()/2,
+                    canvas.winfo_reqwidth()/2, anchor=CENTER, image=new_image)
+
+
+new_game_button = Button(canvas, bg="blue", fg="blue", text="New Game")
+new_game_button_window = canvas.create_window(gap_from_edge + 6*(BOARD_SQUARE['spacer']+BOARD_SQUARE['width']), 2*gap_from_top + 8*(
+    BOARD_SQUARE['spacer']+BOARD_SQUARE['width']), anchor=W, window=new_game_button, width=2 * BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'], height=BOARD_SQUARE['width']/2)
+
+quit_button = Button(canvas, bg="blue", fg="red", text="Exit", command=quit)
+quit_button_window = canvas.create_window(gap_from_edge + 6*(BOARD_SQUARE['spacer']+BOARD_SQUARE['width']), 2*gap_from_top + 8*(
+    BOARD_SQUARE['spacer']+BOARD_SQUARE['width']) + BOARD_SQUARE['width']/2 + 2*BOARD_SQUARE['spacer'], anchor=W, window=quit_button, width=2 * BOARD_SQUARE['width'] + BOARD_SQUARE['spacer'], height=BOARD_SQUARE['width']/2)
+
+current_player_label = Label(canvas, text="Player:", font=Font(
+    family="Cambria", size=20), background="white", foreground="black")
+current_player_label.place(relx=2*gap_from_edge, rely=2 *
+                           gap_from_top + 8*(BOARD_SQUARE['spacer']+BOARD_SQUARE['width']), anchor=W)
+canvas.pack(fill=NONE)
+
+window.mainloop()
--- The Roman Stones.py
+++ The Roman Stones.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def validate_user_input(msg, options):
     while True:
         user_input = input(msg)
@@ -7,8 +8,10 @@ def validate_user_input(msg, options):
             return user_input
         print("Not a valid entry!\n")
 
+
 def roll_die():
-    return randint(1,6)
+    return randint(1, 6)
+
 
 def validate_move(current_index, num, colour):
     if board[current_index].count(colour) == 0:
@@ -20,26 +23,29 @@ def validate_move(current_index, num, colour):
         return False
     return True
 
+
 def move_piece(current_index, num, colour):
     board[current_index].remove(colour)
     if current_index + num == 28:
         finished_tokens.append(colour)
     else:
         board[current_index + num].append(colour)
-    for i in range(current_index+1,current_index+num):
-        if board[i] and i not in (7,14,21):
+    for i in range(current_index+1, current_index+num):
+        if board[i] and i not in (7, 14, 21):
             for piece in board[i]:
                 if piece != colour:
                     board[i].remove(piece)
                     board[0].append(piece)
-    #Might cause errors at end of board
+    # Might cause errors at end of board
     return True
+
 
 def print_board(board):
     spaces = []
     for place in board:
         if place:
-            spaces.append(int((6-len(place))/2)*" " + "".join(place) + (6-len(place)-int((6-len(place))/2))*" ")
+            spaces.append(int((6-len(place))/2)*" " + "".join(place) +
+                          (6-len(place)-int((6-len(place))/2))*" ")
         else:
             spaces.append(6*" ")
     board_graphics = """
@@ -71,21 +77,26 @@ def print_board(board):
      |______|______|______|______|______|______|______|______|
 
        (s4)    19     20     21     22     23     24    (s1)
-""".format(*spaces[::-1][13:21], spaces[15], spaces[6], spaces[16], spaces[5], spaces[17],spaces[4], spaces[18], spaces[3], spaces[19], spaces[2], spaces[20], spaces[1], *spaces[21:28], spaces[0], ",".join(finished_tokens))
+""".format(*spaces[::-1][13:21], spaces[15], spaces[6], spaces[16], spaces[5], spaces[17], spaces[4], spaces[18], spaces[3], spaces[19], spaces[2], spaces[20], spaces[1], *spaces[21:28], spaces[0], ",".join(finished_tokens))
     print(board_graphics)
+
 
 board = [[] for i in range(28)]
 finished_tokens = []
-possible_colours = ["Blue","Green","Red","White"]
-user_num_to_index = {"s1":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"s2":7,"7":8,"8":9,"9":10,"10":11,"11":12,"12":13,"s3":14,"13":15,"14":16,"15":17,"16":18,"17":19,"18":20,"s4":21,"19":22,"20":23,"21":24,"22":25,"23":26,"24":27}
+possible_colours = ["Blue", "Green", "Red", "White"]
+user_num_to_index = {"s1": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "s2": 7, "7": 8, "8": 9, "9": 10, "10": 11, "11": 12, "12": 13,
+                     "s3": 14, "13": 15, "14": 16, "15": 17, "16": 18, "17": 19, "18": 20, "s4": 21, "19": 22, "20": 23, "21": 24, "22": 25, "23": 26, "24": 27}
 players = []
 turn_counter = 1
 
-num_players = int(validate_user_input("Please enter the number of players (2-4): ", "234"))
-num_tokens = int(validate_user_input("How many tokens should each player have? (1-3) ", "123"))
+num_players = int(validate_user_input(
+    "Please enter the number of players (2-4): ", "234"))
+num_tokens = int(validate_user_input(
+    "How many tokens should each player have? (1-3) ", "123"))
 
-for i in range(1,num_players + 1):
-    player_colour = validate_user_input(f"Player {i}, what colour do you want to be? ({', '.join(possible_colours)}) ", possible_colours)
+for i in range(1, num_players + 1):
+    player_colour = validate_user_input(
+        f"Player {i}, what colour do you want to be? ({', '.join(possible_colours)}) ", possible_colours)
     players.append(player_colour)
     possible_colours.remove(player_colour)
     for i in range(num_tokens):
@@ -100,13 +111,14 @@ while not max([finished_tokens.count(colour[0]) for colour in players]) == num_t
     print(die_roll)
     while True:
         move_possible = False
-        for i in range(0,28):
-            if validate_move(i,die_roll,players[turn_counter-1][0]):
+        for i in range(0, 28):
+            if validate_move(i, die_roll, players[turn_counter-1][0]):
                 move_possible = True
         if move_possible:
-            current_index = user_num_to_index[validate_user_input("Which piece would you like to move? (enter place name) ", user_num_to_index.keys())]
-            if validate_move(current_index,die_roll,players[turn_counter-1][0]):
-                move_piece(current_index,die_roll,players[turn_counter-1][0])
+            current_index = user_num_to_index[validate_user_input(
+                "Which piece would you like to move? (enter place name) ", user_num_to_index.keys())]
+            if validate_move(current_index, die_roll, players[turn_counter-1][0]):
+                move_piece(current_index, die_roll, players[turn_counter-1][0])
                 break
             print("Not a valid move")
         else:
