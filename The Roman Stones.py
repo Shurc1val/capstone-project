368690407883e5a567b2c79572d964a2ac34f280
Shurc1val <hughpm16@gmail.com>
2023-10-01 16:33:11 +0100

    still working on meshing graphics and backend

--- The Roman Stones.py
+++ The Roman Stones.py
@@ -1,12 +1,62 @@
 from random import randint
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter.font import Font
+
+from userproof_inputs import ask_user_for_number, ask_user_for_specific_inputs
+from testing_PIL_graphics import *
 
 
-def validate_user_input(msg, options):
-    while True:
-        user_input = input(msg)
-        if user_input in options:
-            return user_input
-        print("Not a valid entry!\n")
+def main():
+
+    board = [[] for i in range(28)]
+    finished_tokens = []
+    possible_colours = ["Blue", "Green", "Red", "Purple"]
+    players = []
+    turn_counter = 1
+
+    window = Tk()
+
+    window.title('The Roman Stones')
+    window.geometry("720x800+10+20")
+    window.configure(bg="black")
+
+    game_settings = {"colours": []}
+    new_game_pop_up_players_and_tokens(window, game_settings)
+
+    new_game_pop_up_colours(window, game_settings, possible_colours)
+    print(game_settings)
+
+    draw_board(window, game_settings['colours'], game_settings['num_counters'])
+
+    window.mainloop()
+
+    """while not max([finished_tokens.count(colour[0]) for colour in players]) == num_counters:
+        print("\n{} player turn".format(players[turn_counter-1]))
+        input("Press any [enter] to roll the die: ")
+        die_roll = roll_die()
+        print(die_roll)
+        while True:
+            move_possible = False
+            for i in range(0, 28):
+                if validate_move(i, die_roll, players[turn_counter-1][0]):
+                    move_possible = True
+            if move_possible:
+                current_index = user_num_to_index[ask_user_for_specific_inputs(
+                    "Which piece would you like to move? (enter place name) ", user_num_to_index.keys())]
+                if validate_move(current_index, die_roll, players[turn_counter-1][0]):
+                    move_piece(current_index, die_roll,
+                               players[turn_counter-1][0])
+                    break
+                print("Not a valid move")
+            else:
+                print("No moves possible!!")
+                input("(press [enter] to continue)")
+                break
+        print_board(board)
+        turn_counter = (turn_counter + 1) % num_players
+
+    print(f"Congratulations {players[turn_counter-1]} player, you win!!")"""
 
 
 def roll_die():
@@ -81,51 +131,5 @@ def print_board(board):
     print(board_graphics)
 
 
-board = [[] for i in range(28)]
-finished_tokens = []
-possible_colours = ["Blue", "Green", "Red", "White"]
-user_num_to_index = {"s1": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "s2": 7, "7": 8, "8": 9, "9": 10, "10": 11, "11": 12, "12": 13,
-                     "s3": 14, "13": 15, "14": 16, "15": 17, "16": 18, "17": 19, "18": 20, "s4": 21, "19": 22, "20": 23, "21": 24, "22": 25, "23": 26, "24": 27}
-players = []
-turn_counter = 1
-
-num_players = int(validate_user_input(
-    "Please enter the number of players (2-4): ", "234"))
-num_tokens = int(validate_user_input(
-    "How many tokens should each player have? (1-3) ", "123"))
-
-for i in range(1, num_players + 1):
-    player_colour = validate_user_input(
-        f"Player {i}, what colour do you want to be? ({', '.join(possible_colours)}) ", possible_colours)
-    players.append(player_colour)
-    possible_colours.remove(player_colour)
-    for i in range(num_tokens):
-        board[0].append(player_colour[0])
-
-print_board(board)
-
-while not max([finished_tokens.count(colour[0]) for colour in players]) == num_tokens:
-    print("\n{} player turn".format(players[turn_counter-1]))
-    input("Press any [enter] to roll the die: ")
-    die_roll = roll_die()
-    print(die_roll)
-    while True:
-        move_possible = False
-        for i in range(0, 28):
-            if validate_move(i, die_roll, players[turn_counter-1][0]):
-                move_possible = True
-        if move_possible:
-            current_index = user_num_to_index[validate_user_input(
-                "Which piece would you like to move? (enter place name) ", user_num_to_index.keys())]
-            if validate_move(current_index, die_roll, players[turn_counter-1][0]):
-                move_piece(current_index, die_roll, players[turn_counter-1][0])
-                break
-            print("Not a valid move")
-        else:
-            print("No moves possible!!")
-            input("(press [enter] to continue)")
-            break
-    print_board(board)
-    turn_counter = (turn_counter + 1) % num_players
-
-print(f"Congratulations {players[turn_counter-1]} player, you win!!")
+if __name__ == "__main__":
+    main()
